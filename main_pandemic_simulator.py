addf5282de48a7c939e730007354bbd53a6288d5
Gabe <soccernerdgj@gmail.com>
2023-04-24 18:25:15 -0600

    added self to all of the options menu stuff

--- main_pandemic_simulator.py
+++ main_pandemic_simulator.py
@@ -119,181 +119,213 @@ class OptionsPage(tk.Frame):
         )
 
         # People options Frame
-        people_options_frame = tk.Frame(self, bg='light blue')
+        self.people_options_frame = tk.Frame(self, bg='light blue')
 
-        people_options_frame.pack(side='left', fill='both', expand=True, anchor='sw')
-        people_options_frame.grid_columnconfigure(0, weight=1)
-        people_options_frame.grid_columnconfigure(1, weight=1)
-        people_options_label = ttk.Label(
-            people_options_frame, text="People Options", anchor='center',style='options_headers.TLabel'
+        self.people_options_frame.pack(side='left', fill='both', expand=True, anchor='sw')
+        self.people_options_frame.grid_columnconfigure(0, weight=1)
+        self.people_options_frame.grid_columnconfigure(1, weight=1)
+        self.people_options_label = ttk.Label(
+            self.people_options_frame, text="People Options", anchor='center',style='options_headers.TLabel'
         )
-        people_options_label.grid(row = 0, column= 0, sticky='nsew', columnspan=2)
+        self.people_options_label.grid(row = 0, column= 0, sticky='nsew', columnspan=2)
 
         # Healthy People
-        health_count_var = tk.StringVar()
-        health_count_label = ttk.Label(
-            people_options_frame,text="Healthy Count: ", style="options_labels.TLabel"
+        self.health_count_var = tk.StringVar()
+        self.health_count_label = ttk.Label(
+            self.people_options_frame,text="Healthy Count: ", style="options_labels.TLabel"
         )
-        health_count_label.grid(row = 1, column = 0, padx= (10, 0), pady = 10, sticky='w')
-        health_count_entry = ttk.Entry(
-            people_options_frame, textvariable=health_count_var
+        self.health_count_label.grid(row = 1, column = 0, padx= (10, 0), pady = 10, sticky='w')
+        self.health_count_entry = ttk.Entry(
+            self.people_options_frame, textvariable=self.health_count_var
         )
-        health_count_entry.grid(row=1, column = 1,padx=(0, 10), sticky='e')
+        self.health_count_entry.grid(row=1, column = 1,padx=(0, 10), sticky='e')
 
         # Asymptomatic People
-        asymp_count_var = tk.StringVar()
-        asymp_count_label = ttk.Label(
-            people_options_frame, text="Asymptomatic Count: ", style= "options_labels.TLabel"
+        self.asymp_count_var = tk.StringVar()
+        self.asymp_count_label = ttk.Label(
+            self.people_options_frame, text="Asymptomatic Count: ", style= "options_labels.TLabel"
         )
-        asymp_count_label.grid(row = 2, column = 0, padx= (10, 0), pady = 10, sticky='w')
-        asymp_count_entry = ttk.Entry(
-            people_options_frame, textvariable=asymp_count_var
+        self.asymp_count_label.grid(row = 2, column = 0, padx= (10, 0), pady = 10, sticky='w')
+        self.asymp_count_entry = ttk.Entry(
+            self.people_options_frame, textvariable=self.asymp_count_var
         )
-        asymp_count_entry.grid(row = 2, column = 1, sticky='e', padx = (0, 10), pady=10)
+        self.asymp_count_entry.grid(row = 2, column = 1, sticky='e', padx = (0, 10), pady=10)
 
         # Symptomatic Peple #
-        symp_count_var = tk.StringVar()
-        symp_count_label = ttk.Label(
-            people_options_frame, text="Symptomatic Count: ", style= "options_labels.TLabel"
+        self.symp_count_var = tk.StringVar()
+        self.symp_count_label = ttk.Label(
+            self.people_options_frame, text="Symptomatic Count: ", style= "options_labels.TLabel"
         )
-        symp_count_label.grid(row = 3, column = 0, padx= (10, 0), pady = 10, sticky='w')
-        symp_count_entry = ttk.Entry(
-            people_options_frame, textvariable=symp_count_var
+        self.symp_count_label.grid(row = 3, column = 0, padx= (10, 0), pady = 10, sticky='w')
+        self.symp_count_entry = ttk.Entry(
+            self.people_options_frame, textvariable=self.symp_count_var
         )
-        symp_count_entry.grid(row = 3, column = 1, padx=(0, 10),pady= 10, sticky='e' )
+        self.symp_count_entry.grid(row = 3, column = 1, padx=(0, 10),pady= 10, sticky='e' )
 
 
         # Social Factor #
-        social_factor_var = tk.StringVar()
-        social_factor_label = ttk.Label(
-            people_options_frame, text="Symptomatic Count: ", style= "options_labels.TLabel"
+        self.social_factor_var = tk.StringVar()
+        self.social_factor_label = ttk.Label(
+            self.people_options_frame, text="Social Factor: ", style= "options_labels.TLabel"
         )
-        social_factor_label.grid(row = 4, column = 0, padx= (10, 0), pady = 10, sticky='w')
-        social_factor_entry = ttk.Entry(
-            people_options_frame, textvariable=social_factor_var
+        self.social_factor_label.grid(row = 4, column = 0, padx= (10, 0), pady = 10, sticky='w')
+        self.social_factor_entry = ttk.Entry(
+            self.people_options_frame, textvariable=self.social_factor_var
         )
-        social_factor_entry.grid(row = 4, column = 1, padx=(0, 10),pady= 10, sticky='e' )
+        self.social_factor_entry.grid(row = 4, column = 1, padx=(0, 10),pady= 10, sticky='e' )
 
 
 
         # pathogen options Frame
-        patho_options_frame = tk.Frame(self, bg='light blue', height=HEIGHT)
-        patho_options_frame.pack(side='left', fill='both', expand=True)
-        patho_options_frame.grid_columnconfigure(0, weight=1)
-        patho_options_frame.grid_columnconfigure(1, weight=1)
-        patho_header_label = ttk.Label(
-            patho_options_frame, text='Pathogen Options', anchor='center', style='options_headers.TLabel',
+        self.patho_options_frame = tk.Frame(self, bg='light blue', height=HEIGHT)
+        self.patho_options_frame.pack(side='left', fill='both', expand=True)
+        self.patho_options_frame.grid_columnconfigure(0, weight=1)
+        self.patho_options_frame.grid_columnconfigure(1, weight=1)
+        self.patho_header_label = ttk.Label(
+            self.patho_options_frame, text='Pathogen Options', anchor='center', style='options_headers.TLabel',
         )
-        patho_header_label.grid(row=0, column=0, sticky='nsew', columnspan=2)
+        self.patho_header_label.grid(row=0, column=0, sticky='nsew', columnspan=2)
 
         # Pathogen Type #
-        path_type_var = tk.StringVar()
-        path_type_label = ttk.Label(
-            patho_options_frame, text="Type: ", style='options_labels.TLabel',
+        self.path_type_var = tk.StringVar()
+        self.path_type_label = ttk.Label(
+            self.patho_options_frame, text="Type: ", style='options_labels.TLabel',
         )
-        path_type_label.grid(row=1, column = 0, sticky='w', padx=(10, 0), pady=10)
-        path_types = ['Bacteria', 'Virus', 'Protazoa', 'Prion', 'Fungus']
-        path_type_choices = ttk.OptionMenu(
-            patho_options_frame, path_type_var,*path_types,
+        self.path_type_label.grid(row=1, column = 0, sticky='w', padx=(10, 0), pady=10)
+        self.path_types = ['Bacteria', 'Virus', 'Protazoa', 'Prion', 'Fungus']
+        self.path_type_choices = ttk.OptionMenu(
+            self.patho_options_frame, self.path_type_var,*self.path_types,
         )
-        path_type_choices.grid(row=1, column = 1, sticky='e', padx=(0, 10))
+        self.path_type_choices.grid(row=1, column = 1, sticky='e', padx=(0, 10))
 
         # Pathogen Name #
-        path_name_var = tk.StringVar()
-        path_name_label = ttk.Label(
-            patho_options_frame, text = "Name: ", style='options_labels.TLabel'
+        self.path_name_var = tk.StringVar()
+        self.path_name_label = ttk.Label(
+            self.patho_options_frame, text = "Name: ", style='options_labels.TLabel'
         )
-        path_name_label.grid(row=2, column = 0, sticky='w', padx=(10, 0), pady=10)
-        path_name_entry = ttk.Entry(
-            patho_options_frame, textvariable=path_name_var
+        self.path_name_label.grid(row=2, column = 0, sticky='w', padx=(10, 0), pady=10)
+        self.path_name_entry = ttk.Entry(
+            self.patho_options_frame, textvariable=self.path_name_var
         )
-        path_name_entry.grid(row = 2, column = 1, sticky='e', padx=(0, 10), pady=10)
+        self.path_name_entry.grid(row = 2, column = 1, sticky='e', padx=(0, 10), pady=10)
 
 
         # Damage Factor #
-        damage_factor_var = tk.StringVar()
-        damage_factor_label = ttk.Label(
-            patho_options_frame, text = "Damage Factor: ", style='options_labels.TLabel'
+        self.damage_factor_var = tk.StringVar()
+        self.damage_factor_label = ttk.Label(
+            self.patho_options_frame, text = "Damage Factor: ", style='options_labels.TLabel'
         )
-        damage_factor_label.grid(row=3, column = 0, sticky='w', padx=(10, 0), pady=10)
-        damage_factor_entry = ttk.Entry(
-            patho_options_frame, textvariable=damage_factor_var
+        self.damage_factor_label.grid(row=3, column = 0, sticky='w', padx=(10, 0), pady=10)
+        self.damage_factor_entry = ttk.Entry(
+            self.patho_options_frame, textvariable=self.damage_factor_var
         )
-        damage_factor_entry.grid(row = 3, column = 1, sticky='e', padx=(0, 10), pady=10)
+        self.damage_factor_entry.grid(row = 3, column = 1, sticky='e', padx=(0, 10), pady=10)
 
 
         # Cure Defense
-        cure_defense_var = tk.StringVar()
-        cure_defense_label = ttk.Label(
-            patho_options_frame, text = "Cure Defense: ", style='options_labels.TLabel'
+        self.cure_defense_var = tk.StringVar()
+        self.cure_defense_label = ttk.Label(
+            self.patho_options_frame, text = "Cure Defense: ", style='options_labels.TLabel'
         )
-        cure_defense_label.grid(row=4, column = 0, sticky='w', padx=(10, 0), pady=10)
-        cure_defense_entry = ttk.Entry(
-            patho_options_frame, textvariable=cure_defense_var
+        self.cure_defense_label.grid(row=4, column = 0, sticky='w', padx=(10, 0), pady=10)
+        self.cure_defense_entry = ttk.Entry(
+            self.patho_options_frame, textvariable=self.cure_defense_var
         )
-        cure_defense_entry.grid(row = 4, column = 1, sticky='e', padx=(0, 10), pady=10)
+        self.cure_defense_entry.grid(row = 4, column = 1, sticky='e', padx=(0, 10), pady=10)
 
 
         # Incubation Length #
-        incubation_len_var = tk.StringVar()
-        incubation_len_label = ttk.Label(
-            patho_options_frame, text = "Incubation Length: ", style='options_labels.TLabel'
+        self.incubation_len_var = tk.StringVar()
+        self.incubation_len_label = ttk.Label(
+            self.patho_options_frame, text = "Incubation Length: ", style='options_labels.TLabel'
         )
-        incubation_len_label.grid(row=5, column = 0, sticky='w', padx=(10, 0), pady=10)
-        incubation_len_entry = ttk.Entry(
-            patho_options_frame, textvariable=incubation_len_var
+        self.incubation_len_label.grid(row=5, column = 0, sticky='w', padx=(10, 0), pady=10)
+        self.incubation_len_entry = ttk.Entry(
+            self.patho_options_frame, textvariable=self.incubation_len_var
         )
-        incubation_len_entry.grid(row = 5, column = 1, sticky='e', padx=(0, 10), pady=10)
+        self.incubation_len_entry.grid(row = 5, column = 1, sticky='e', padx=(0, 10), pady=10)
 
 
         # Symptomatic Infectability #
-        symptom_infect_var = tk.StringVar()
-        symptom_infect_label = ttk.Label(
-            patho_options_frame, text = "Symptomatic Power: ", style='options_labels.TLabel'
+        self.symptom_infect_var = tk.StringVar()
+        self.symptom_infect_label = ttk.Label(
+            self.patho_options_frame, text = "Symptomatic Power: ", style='options_labels.TLabel'
         )
-        symptom_infect_label.grid(row=6, column = 0, sticky='w', padx=(10, 0), pady=10)
-        symptom_infect_entry = ttk.Entry(
-            patho_options_frame, textvariable=symptom_infect_var
+        self.symptom_infect_label.grid(row=6, column = 0, sticky='w', padx=(10, 0), pady=10)
+        self.symptom_infect_entry = ttk.Entry(
+            self.patho_options_frame, textvariable=self.symptom_infect_var
         )
-        symptom_infect_entry.grid(row = 6, column = 1, sticky='e', padx=(0, 10), pady=10)
+        self.symptom_infect_entry.grid(row = 6, column = 1, sticky='e', padx=(0, 10), pady=10)
 
 
 
         # Asymptomatic Infectability #
-        asymptom_infect_var = tk.StringVar()
-        asymptom_infect_label = ttk.Label(
-            patho_options_frame, text = "Asymptomatic Power: ", style='options_labels.TLabel'
+        self.asymptom_infect_var = tk.StringVar()
+        self.asymptom_infect_label = ttk.Label(
+            self.patho_options_frame, text = "Asymptomatic Power: ", style='options_labels.TLabel'
         )
-        asymptom_infect_label.grid(row=7, column = 0, sticky='w', padx=(10, 0), pady=10)
-        asymptom_infect_entry = ttk.Entry(
-            patho_options_frame, textvariable=asymptom_infect_var
+        self.asymptom_infect_label.grid(row=7, column = 0, sticky='w', padx=(10, 0), pady=10)
+        self.asymptom_infect_entry = ttk.Entry(
+            self.patho_options_frame, textvariable=self.asymptom_infect_var
         )
-        asymptom_infect_entry.grid(row = 7, column = 1, sticky='e', padx=(0, 10), pady=10)
+        self.asymptom_infect_entry.grid(row = 7, column = 1, sticky='e', padx=(0, 10), pady=10)
 
 
 
-        # Surface Infectability Factor
-        surface_infect_var = tk.StringVar()
-        surface_infect_label = ttk.Label(
-            patho_options_frame, text = "Surface Power: ", style='options_labels.TLabel'
+        # Surface Infectability Factor #
+        self.surface_infect_var = tk.StringVar()
+        self.surface_infect_label = ttk.Label(
+            self.patho_options_frame, text = "Surface Power: ", style='options_labels.TLabel'
         )
-        surface_infect_label.grid(row=8, column = 0, sticky='w', padx=(10, 0), pady=10)
-        surface_infect_entry = ttk.Entry(
-            patho_options_frame, textvariable=surface_infect_var
+        self.surface_infect_label.grid(row=8, column = 0, sticky='w', padx=(10, 0), pady=10)
+        self.surface_infect_entry = ttk.Entry(
+            self.patho_options_frame, textvariable=self.surface_infect_var
         )
-        surface_infect_entry.grid(row = 8, column = 1, sticky='e', padx=(0, 10), pady=10)
+        self.surface_infect_entry.grid(row = 8, column = 1, sticky='e', padx=(0, 10), pady=10)
+
+        # Infection Length
+        self.path_len_var = tk.StringVar()
+        self.path_len_label = ttk.Label(
+            self.patho_options_frame, text = "Infection Length ", style='options_labels.TLabel'
+        )
+        self.path_len_label.grid(row=9, column = 0, sticky='w', padx=(10, 0), pady=10)
+        self.path_len_entry = ttk.Entry(
+            self.patho_options_frame, textvariable=self.path_len_var
+        )
+        self.path_len_entry.grid(row = 9, column = 1, sticky='e', padx=(0, 10), pady=10)
 
 
         # Run Simulation Button
-        run_sim_button = ttk.Button(
-            people_options_frame, text="Run Simulation", style='run.TButton', command=self.run_simulation
+        self.run_sim_button = ttk.Button(
+            self.people_options_frame, text="Run Simulation", style='run.TButton', command=self.run_simulation
         )
-        run_sim_button.grid(row=5, column=0, sticky='sw')
-        people_options_frame.grid_rowconfigure(5, weight=7)
+        self.run_sim_button.grid(row=5, column=0, sticky='sw')
+        self.people_options_frame.grid_rowconfigure(5, weight=7)
+
+    def generate_population(self):
+        healthy_people = int(self.health_count_entry.get()) if self.health_count_entry.get().isnumeric() else 0
+        asymp_people = int(self.asymp_count_entry.get()) if self.asymp_count_entry.get().isnumeric() else 0
+        symp_people = int(self.symp_count_entry.get()) if self.symp_count_entry.get().isnumeric() else 0
+        field.populate(healthy_people, asymp_people, symp_people)
+
+    def generate_virus(self):
+        pathogen_name = self.path_name_entry.get()
+        pathogen_type = self.path_type_var.get()
+        symptomatic_infectability = int(self.symptom_infect_entry.get()) if self.symptom_infect_entry.get().isnumeric() else 0
+        asymptomati_infectability = int(self.asymptom_infect_entry.get()) if self.asymptom_infect_entry.get().isnumeric() else 0
+        longevity_factor = int(self.path_len_entry.get()) if self.path_len_entry.get().isnumeric() else 0
+
+
+        field.add_pathogen(
+            type=pathogen.Baddy(pathogen_type),
+            name = pathogen_name,
+            symptomatic_infectability=symptomatic_infectability,
+            asymptomatic_infectability=asymptomati_infectability,
+            longevity_factor=longevity_factor,
+        )
 
     def run_simulation(self):
         pass
-
 
 
 
